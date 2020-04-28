24fe5cfe7f10bd6c10c5345d7a508b7f8833a95f
AhmadLobany <ahmad.lobany@campus.technion.ac.il>
2020-04-28 11:15:53 +0300

    commit

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
-
-
 const source = $('#apart-template').html();
-const template = Handlebars.compile(source)
+const template = Handlebars.compile(source);
 
 
 $("button").on("click", function () {
-    let address = $("#addr-input").val()
-    let minPrice = $("#min-p-input").val()
-    let maxPrice = $("#max-p-input").val()
-    let minRooms = $("#min-r-input").val()
-    let maxRooms = $("#max-r-input").val()
-    let immediate = $("#immed-y")
-    let parking = $('#park-y').is(":checked")
-    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate,parking)
+    const address = $("#addr-input").val()
+    const minPrice = $("#min-p-input").val()
+    const maxPrice = $("#max-p-input").val()
+    const minRooms = $("#min-r-input").val()
+    const maxRooms = $("#max-r-input").val()
+    const immediate = $("#immed-y")
+    const parking = $('#park-y').is(":checked")
+    const relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate, parking)
     renderApts(relevantApts)
 })
 
+
+
 const renderApts = function (apartments) {
     $("#results").empty();
-    const newHTML = template({data: apartments});
+    const newHTML = template({ data: apartments });
     $('#results').append(newHTML);
-    if(apartments=="") alert('No Matching Results')
+    if (apartments == "") alert('No Matching Results') //If there are no apartments
 }
 
-renderApts(apartments) //renders apartments when page loads
+
+renderApts(apartments) //Renders apartments when page loads for first time!
